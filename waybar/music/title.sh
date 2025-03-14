d7ffde0c60763b7883facbd333912b0623868aa9
Hector Roussel <hector.roussel@rowlback.com>
2025-03-14 11:07:55 +0100

    fix chars on title i guess

--- waybar/music/title.sh
+++ waybar/music/title.sh
@@ -4,7 +4,7 @@ raw_title=$(playerctl metadata title)
 artist=$(playerctl metadata artist)
 arturl=$(playerctl metadata mpris:artUrl)
 
-# reformat title
+# Reformat title
 title=$(echo "$raw_title" | sed \
     -e 's/\\/\\\\/g' \
     -e 's/"/\\"/g' \
@@ -12,19 +12,19 @@ title=$(echo "$raw_title" | sed \
     -e 's/`/\\`/g' \
     -e 's/\$/\\\$/g' \
     -e 's/\*/\\*/g' \
-    -e 's/&/\\&/g' \
+    -e 's/\//\\\//g' \
     -e 's/|/\\|/g' \
     -e 's/;/\\;/g')
 
-# calculate timings
+# Calculate timings
 rawposition=$(playerctl position)
 rawlength=$(echo "$(playerctl metadata mpris:length) / 1000000" | bc)
 
-positionhours=$(echo "$raw_position / 3600" | bc)
+positionhours=$(echo "$rawposition / 3600" | bc)
 positionminutes=$(printf "%02d" $(echo "($rawposition % 3600) / 60" | bc))
 positionseconds=$(printf "%02d" $(echo "$rawposition % 60" | bc))
 
-lengthhours=$(echo "$raw_length / 3600" | bc)
+lengthhours=$(echo "$rawlength / 3600" | bc)
 lengthminutes=$(printf "%02d" $(echo "($rawlength % 3600) / 60" | bc))
 lengthseconds=$(printf "%02d" $(echo "$rawlength % 60" | bc))
 
@@ -36,6 +36,12 @@ else
     length="${lengthminutes}:${lengthseconds}"
 fi
 
-# Echo informations
-echo "{\"text\": \"$title\", \"tooltip\": \"$title\n$artist\n$position / $length\"}"
+# Calculate progress bar
+progress=$(echo "($rawposition * 50) / $rawlength" | bc)
+bar=$(printf "█%.0s" $(seq 1 $progress))
+empty=$(printf "░%.0s" $(seq 1 $((50 - $progress))))
+progressbar="$bar$empty"
+
+# Echo information with progress bar
+echo "{\"text\": \"$title\", \"tooltip\": \"$title\n$artist\n\n[$progressbar]\n$position / $length\"}"
 
